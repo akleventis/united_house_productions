54c6fa910b9b20fd17afd05d3ff3652c1bcad239
Burk Manson <burkmanson@gmail.com>
2022-03-12 13:26:43 -0800

    Basic shopping cart modal

--- src/headers/Navigate.js
+++ src/headers/Navigate.js
@@ -1,36 +1,44 @@
 // import {LinkTree, Instagram, Facebook } from '../assets/images/index'
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { ShoppingCart } from "../components/index.js";
 import "./headers.css";
 
-const Navigate = () => {
+const Navigate = ({ showShoppingCart, setShowShoppingCart }) => {
   return (
-    <Navbar expand="md navbar-dark" className="nav-links">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              Events
-            </Nav.Link>
-            <Nav.Link as={Link} to="/merch">
-              Merch
-            </Nav.Link>
-            <Nav.Link as={Link} to="/about">
-              About
-            </Nav.Link>
-            <Nav.Link as={Link} to="/contact">
-              Contact
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <div>
-          {/* <img src={LinkTree} alt='link tree' />
+    <>
+      <Navbar expand="md navbar-dark" className="nav-links">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Events
+              </Nav.Link>
+              <Nav.Link as={Link} to="/merch">
+                Merch
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/contact">
+                Contact
+              </Nav.Link>
+              <Button onClick={() => setShowShoppingCart(true)}>🛒</Button>
+            </Nav>
+          </Navbar.Collapse>
+          <div>
+            {/* <img src={LinkTree} alt='link tree' />
                         <img src={Instagram} alt='instagram' />
                         <img src={Facebook} alt='facebook' /> */}
-        </div>
-      </Container>
-    </Navbar>
+          </div>
+        </Container>
+      </Navbar>
+      <ShoppingCart
+        showShoppingCart={showShoppingCart}
+        setShowShoppingCart={setShowShoppingCart}
+      />
+    </>
   );
 };
 
