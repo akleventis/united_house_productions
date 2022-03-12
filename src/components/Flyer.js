0cfd78bbacfe960e921403997e480f1eee8ad06b
Burk Manson <burkmanson@gmail.com>
2022-03-12 12:07:24 -0800

    Made Flyer more modular

--- src/components/Flyer.js
+++ src/components/Flyer.js
@@ -1,12 +1,12 @@
-import { Domii } from "../assets/images/index";
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
 import "./Flyer.css";
+import moment from "moment";
 
-const Flyer = () => {
+const Flyer = ({ event }) => {
   return (
     <div>
-      <p>March 2022</p>
+      <p>{moment(event.startTime).format("MMMM YYYY")}</p>
       <hr />
       <div className="flyer-container">
         <div className="item">
@@ -14,17 +14,21 @@ const Flyer = () => {
             <div>
               <div className="flyer-date">
                 <div></div>
-                <p>Sat, Apr. 16</p>
+                <p>{moment(event.startTime).format("ddd, MMM. DD")}</p>
               </div>
-              <img alt="dj" src={Domii} width="100%" height="100%" />
+              <img alt="dj" src={event.poster} width="100%" height="100%" />
             </div>
           </Zoom>
         </div>
         <div className="item-center">
           <ul className="list-group">
-            <li className="list-group-item headliner">DOMii</li>
+            <li className="list-group-item headliner">
+              {event.headliner.name}
+            </li>
             <li className="list-group-item support">
-              DUPLEX | YESPEEZ | VLADISLOVE | WARRIORS
+              {event.supportingActs
+                .map((act) => act.name.toUpperCase())
+                .join(" | ")}
             </li>
             <li className="list-group-item time">
               <svg
@@ -39,21 +43,17 @@ const Flyer = () => {
               >
                 <path d="M 12 2 C 6.4889971 2 2 6.4889971 2 12 C 2 17.511003 6.4889971 22 12 22 C 17.511003 22 22 17.511003 22 12 C 22 6.4889971 17.511003 2 12 2 z M 12 4 C 16.430123 4 20 7.5698774 20 12 C 20 16.430123 16.430123 20 12 20 C 7.5698774 20 4 16.430123 4 12 C 4 7.5698774 7.5698774 4 12 4 z M 11 6 L 11 12.414062 L 15.292969 16.707031 L 16.707031 15.292969 L 13 11.585938 L 13 6 L 11 6 z"></path>
               </svg>
-              9pm - 2:30am
+              {moment(event.startTime).format("h:mma")} -{" "}
+              {moment(event.endTime).format("h:mma")}
             </li>
-
             <li className="list-group-item location">
-              <a
-                href="https://www.google.com/maps/place/The+Fruit/@35.9906289,-78.8987875,17z/data=!3m1!4b1!4m5!3m4!1s0x89ace46f3381f42d:0x42006ab88a52e967!8m2!3d35.9906823!4d-78.8966525"
-                target="_blank"
-                rel="noreferrer"
-              >
+              <a href={event.location.url} target="_blank" rel="noreferrer">
                 <img
                   src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABmJLR0QA/wD/AP+gvaeTAAACCklEQVQ4jYWQv08TcRjGn7vDg7uj0F5NE7QhkLAYaa2JPxIHg4bAQoQwOOvgogkLE5MuRiZm9T8wKWlITYpDxcREJzFQRLrYllJ69Nqzvev9FPw6lDak7eEzffO8z/P5vnkpdNHW1JTADAw8oTkuBAB/TTNl2fbbG/G40Z6lOsrz8w88gcDLwOjoOMOyAIATx0Epk9lRy+WlSDT63hWQmp29I46NvRODwWC3zSr7+wdKNvswHIt9bXr02UCvz/e8WdZ1HSXQkCkautHY3D88HOwbHHxxttMCfJmeFgVRDAOAY9swr4bge7YA79MFmFfG8ce2AQD9ohjamJjwdgAIIZdYnvcDQN004bk32frFc38SdcMEAFzg+YsCRV3uAJwwzNGx46gAwLEsjNR2C2Bsb4HrbRz02HFqhKal5qyn+bibSMi/RkYK3qEhP8fzsD4l8Tv9EwDASkX0CTwAwNK0g9vJZKUDAACOrn8DIWFQFHz9AqDVGoPTMgiBWa9vdj0iAGiatqKWSjJcVJOkslOtrrgCbsZiO0qh8NkNUC0WNyLx+A9XAABYsrxYyeVy7X45k8mpqrrY7ncArq+vZ5V8/rWt61YLqut29fDwza21tfx/AQAQXl1dltLpD4QQEEJwtLeXCEWjr7ple7qZACArymNmd/cjRVFM3bYfueXO1fe5ucjmzMy18zL/AN320eTh4osMAAAAAElFTkSuQmCC"
                   alt="location"
                 />
               </a>
-              The Fruit
+              {event.location.name}
             </li>
           </ul>
         </div>
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,7 +1,8 @@
 import Flyer from "../components/Flyer.js";
+import { Domii } from "../assets/images/index";
 import "./events.css";
 
-let djs = {
+const djs = {
   domii: {
     name: "DOMii",
     site: "",
@@ -46,34 +47,38 @@ let djs = {
   },
 };
 
-const events = {
-  domii: {
+const events = [
+  {
     headliner: djs.domii,
-    support: [djs.duplex, djs.yespeez, djs.vladislove, djs.warriors],
-    time: "9pm - 2:30am",
+    poster: Domii,
+    supportingActs: [djs.duplex, djs.yespeez, djs.vladislove, djs.warriors],
     location: {
+      name: "The Fruit",
       url: "https://www.google.com/maps/place/The+Fruit/@35.9906289,-78.8987875,17z/data=!3m1!4b1!4m5!3m4!1s0x89ace46f3381f42d:0x42006ab88a52e967!8m2!3d35.9906823!4d-78.8966525",
-      name: "The Fruit"
     },
+    startTime: "2022-04-16T21:00:00.000",
+    endTime: "2022-04-17T02:30:00.000",
   },
-  next: {
+  {
     headliner: djs.tylr,
-    support: [djs.duplex, djs.yespeez, djs.vladislove, djs.warriors],
-    time: "9pm - 2:30am",
+    poster: null,
+    supportingActs: [djs.duplex, djs.yespeez, djs.vladislove, djs.warriors],
     location: {
       url: "https://www.google.com/maps/place/The+Fruit/@35.9906289,-78.8987875,17z/data=!3m1!4b1!4m5!3m4!1s0x89ace46f3381f42d:0x42006ab88a52e967!8m2!3d35.9906823!4d-78.8966525",
-      name: "The Fruit"
+      name: "The Fruit",
     },
+    startTime: "2022-04-17T21:00:00.000",
+    endTime: "2022-04-18T02:30:00.000",
   },
-};
+];
 
 const Events = (props) => {
   return (
     <div className="events-container">
       <h2>Upcoming Events</h2>
-      <Flyer {...props.events}/>
-      <Flyer {...props.events}/>
-      <Flyer {...props.events}/>
+      {events.map((event, i) => {
+        return <Flyer key={i} event={event} />;
+      })}
     </div>
   );
 };
