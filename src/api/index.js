a3d93ff6743c9babd55be3a0b51f26a9bbb23e6c
Burk Manson <burkmanson@gmail.com>
2022-03-13 16:08:51 -0700

    Very, very basic cart

--- src/api/index.js
+++ src/api/index.js
@@ -12,3 +12,29 @@ export const getEvents = () => getFromData("events");
 export const getFeaturedSongs = () => getFromData("featuredSongs");
 
 export const getProducts = () => getFromData("products");
+
+export const getFromLocalStorage = (key) =>
+  new Promise((res) => res(JSON.parse(localStorage.getItem(key))));
+
+export const setInLocalStorage = (key, data) =>
+  localStorage.setItem(key, JSON.stringify(data));
+
+export const addToCart = async (product) => {
+  const cart = await getFromLocalStorage("cart");
+  if (cart === null) {
+    setInLocalStorage("cart", [product]);
+  } else {
+    const [matchingProduct] = cart.filter((item) => item.id === product.id);
+    if (matchingProduct) {
+      const index = cart.map((item) => item.id).indexOf(product.id);
+      matchingProduct.quantity = String(
+        Number(matchingProduct.quantity) + Number(product.quantity)
+      );
+      cart[index] = matchingProduct;
+
+      setInLocalStorage("cart", cart);
+    } else {
+      setInLocalStorage("cart", [...cart, product]);
+    }
+  }
+};
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,6 +1,18 @@
-import { Modal } from "react-bootstrap";
+import { Modal, ListGroup, Button } from "react-bootstrap";
+import { useState, useEffect, useCallback } from "react";
+import { getFromLocalStorage } from "../api";
+import { BsFillTrashFill } from "react-icons/bs";
 
 const ShoppingCart = ({ showShoppingCart, setShowShoppingCart }) => {
+  const [cart, setCart] = useState([]);
+  const hydrateCart = useCallback(() => {
+    getFromLocalStorage("cart").then((res) => setCart(res ?? []));
+  }, [setCart]);
+
+  useEffect(() => {
+    hydrateCart();
+  }, [hydrateCart, showShoppingCart]);
+
   const handleClose = () => setShowShoppingCart(false);
 
   return (
@@ -8,7 +20,27 @@ const ShoppingCart = ({ showShoppingCart, setShowShoppingCart }) => {
       <Modal.Header closeButton>
         <Modal.Title>Cart</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Something for now</Modal.Body>
+      <Modal.Body>
+        <ListGroup>
+          {cart.map((product, i) => (
+            <ListGroup.Item
+              key={i}
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <span>{product.name}</span>
+              <span>{`QTY: ${product.quantity}`}</span>
+              <Button variant="danger" onClick={() => {}}>
+                {/* TODO: Make this do something */}
+                <BsFillTrashFill />
+              </Button>
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Modal.Body>
     </Modal>
   );
 };
