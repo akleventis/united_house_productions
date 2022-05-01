2778af463205f3160d90967a7f23b83c8373f422
Alex Leventis <alex.leventis@bit.ly>
2022-05-01 12:22:04 -0400

    images

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+// header
+export const UnitedLogo = require('./header/uhp.png')
+export const Background = require('./header/background.jpg')
+
+// social
+export const LinkTree = require('./social/link_tree.png')
+export const Instagram = require('./social/instagram.png')
+export const Facebook = require('./social/facebook.png')
+export const SoundcloudImg = require('./social/soundcloud.png')
+
+// UPDATE FLOW //
+// flyers
+export const Domii = require('./flyers/domii.jpeg')
+export const Patty = require('./flyers/patty.jpg')
+
+// products
+export const Shirt = require('./products/shirt.png')
+export const Shroomy = require('./products/shroomy.png')
+
